0bb568d42d272a52faa4f617aeb97a7fe52c9c10
Aditya Singh <adityasinghrajput755@gmail.com>
2025-08-11 12:49:02 +0530

    Add read_document tool to retrieve document content

--- mcp_server.py
+++ mcp_server.py
@@ -1,3 +1,4 @@
+from pydantic import Field
 from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
@@ -13,6 +14,18 @@ docs = {
 }
 
 # TODO: Write a tool to read a doc
+@mcp.tool (
+    name = "read_doc_content",
+    description= " Read the content of a document and return it as a string"
+)
+
+def read_document (
+    doc_id: str = Field(description = "Id of the document to read")
+):
+    if doc_id not in docs :
+        raise ValueError(f"Doc with id {doc_id} not found")
+    
+    return docs[doc_id]
 # TODO: Write a tool to edit a doc
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
